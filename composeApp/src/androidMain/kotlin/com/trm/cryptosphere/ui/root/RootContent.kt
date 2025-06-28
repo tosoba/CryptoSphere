7d8bda1e23d572f62b55d1eed285465fbfd6a585
merengue <therealmererengue@proton.me>
2025-06-28 14:27:29 +0200

    Use predictiveBackAnimation in RootContent Children

--- composeApp/src/androidMain/kotlin/com/trm/cryptosphere/ui/root/RootContent.kt
+++ composeApp/src/androidMain/kotlin/com/trm/cryptosphere/ui/root/RootContent.kt
@@ -4,14 +4,30 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import com.arkivanov.decompose.ExperimentalDecomposeApi
 import com.arkivanov.decompose.extensions.compose.stack.Children
+import com.arkivanov.decompose.extensions.compose.stack.animation.fade
+import com.arkivanov.decompose.extensions.compose.stack.animation.plus
+import com.arkivanov.decompose.extensions.compose.stack.animation.predictiveback.predictiveBackAnimation
+import com.arkivanov.decompose.extensions.compose.stack.animation.scale
+import com.arkivanov.decompose.extensions.compose.stack.animation.stackAnimation
 import com.trm.cryptosphere.ui.home.HomeContent
 import com.trm.cryptosphere.ui.token.TokenContent
 
 @Composable
 fun RootContent(component: RootComponent) {
   MaterialTheme {
-    Children(stack = component.stack, modifier = Modifier.fillMaxSize()) { child ->
+    Children(
+      stack = component.stack,
+      modifier = Modifier.fillMaxSize(),
+      animation =
+        @OptIn(ExperimentalDecomposeApi::class)
+        predictiveBackAnimation(
+          backHandler = component.backHandler,
+          fallbackAnimation = stackAnimation(fade() + scale()),
+          onBack = component::onBackClicked,
+        ),
+    ) { child ->
       when (val instance = child.instance) {
         is RootComponent.Child.Home -> {
           HomeContent(component = instance.component, modifier = Modifier.fillMaxSize())
