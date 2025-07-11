b8ed09a42a0e68d0c20bd8f347bc397264c73933
merengue <therealmererengue@proton.me>
2025-07-11 20:46:49 +0200

    Add gradient overlays to feed content

--- composeApp/src/androidMain/kotlin/com/trm/cryptosphere/ui/home/HomeContent.kt
+++ composeApp/src/androidMain/kotlin/com/trm/cryptosphere/ui/home/HomeContent.kt
@@ -3,6 +3,7 @@ package com.trm.cryptosphere.ui.home
 import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.asPaddingValues
@@ -15,12 +16,15 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.capitalize
 import androidx.compose.ui.text.intl.LocaleList
 import com.arkivanov.decompose.extensions.compose.pages.ChildPages
 import com.arkivanov.decompose.extensions.compose.pages.PagesScrollAnimation
 import com.arkivanov.decompose.extensions.compose.subscribeAsState
+import com.trm.cryptosphere.core.ui.BottomGradientOverlay
+import com.trm.cryptosphere.core.ui.TopGradientOverlay
 import com.trm.cryptosphere.core.ui.navigationSuiteLayoutType
 import com.trm.cryptosphere.ui.home.page.history.HistoryContent
 import com.trm.cryptosphere.ui.home.page.news.feed.NewsFeedContent
@@ -52,41 +56,51 @@ fun SharedTransitionScope.HomeContent(
       }
     },
   ) {
-    ChildPages(
-      pages = pages,
-      onPageSelected = {},
-      pager = { modifier, state, key, pageContent ->
-        HorizontalPager(
-          modifier = modifier,
-          state = state,
-          key = key,
-          userScrollEnabled = false,
-          pageContent = pageContent,
-        )
-      },
-      modifier =
-        Modifier.fillMaxSize()
-          .padding(WindowInsets.safeDrawing.only(WindowInsetsSides.Top).asPaddingValues()),
-      scrollAnimation = PagesScrollAnimation.Default,
-    ) { _, page ->
-      when (page) {
-        is HomeComponent.Page.NewsFeed -> {
-          NewsFeedContent(
-            component = page.component,
-            animatedVisibilityScope = animatedVisibilityScope,
-            modifier = Modifier.fillMaxSize(),
+    Box(modifier = Modifier.fillMaxSize()) {
+      ChildPages(
+        pages = pages,
+        onPageSelected = {},
+        pager = { modifier, state, key, pageContent ->
+          HorizontalPager(
+            modifier = modifier,
+            state = state,
+            key = key,
+            userScrollEnabled = false,
+            pageContent = pageContent,
           )
-        }
-        is HomeComponent.Page.Prices -> {
-          PricesContent(component = page.component, modifier = Modifier.fillMaxSize())
-        }
-        is HomeComponent.Page.Search -> {
-          SearchContent(component = page.component, modifier = Modifier.fillMaxSize())
-        }
-        is HomeComponent.Page.History -> {
-          HistoryContent(component = page.component, modifier = Modifier.fillMaxSize())
+        },
+        modifier =
+          Modifier.fillMaxSize()
+            .padding(WindowInsets.safeDrawing.only(WindowInsetsSides.Top).asPaddingValues()),
+        scrollAnimation = PagesScrollAnimation.Default,
+      ) { _, page ->
+        when (page) {
+          is HomeComponent.Page.NewsFeed -> {
+            NewsFeedContent(
+              component = page.component,
+              animatedVisibilityScope = animatedVisibilityScope,
+              modifier = Modifier.fillMaxSize(),
+            )
+          }
+          is HomeComponent.Page.Prices -> {
+            PricesContent(component = page.component, modifier = Modifier.fillMaxSize())
+          }
+          is HomeComponent.Page.Search -> {
+            SearchContent(component = page.component, modifier = Modifier.fillMaxSize())
+          }
+          is HomeComponent.Page.History -> {
+            HistoryContent(component = page.component, modifier = Modifier.fillMaxSize())
+          }
         }
       }
+
+      TopGradientOverlay(
+        modifier =
+          Modifier.align(Alignment.TopCenter)
+            .padding(WindowInsets.safeDrawing.only(WindowInsetsSides.Top).asPaddingValues())
+      )
+
+      BottomGradientOverlay(modifier = Modifier.align(Alignment.BottomCenter))
     }
   }
 }
--- composeApp/src/androidMain/kotlin/com/trm/cryptosphere/ui/token/feed/TokenFeedContent.kt
+++ composeApp/src/androidMain/kotlin/com/trm/cryptosphere/ui/token/feed/TokenFeedContent.kt
@@ -3,6 +3,7 @@ package com.trm.cryptosphere.ui.token.feed
 import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.rememberPagerState
@@ -11,9 +12,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.trm.cryptosphere.core.ui.BottomGradientOverlay
 import com.trm.cryptosphere.core.ui.TokenCarousel
+import com.trm.cryptosphere.core.ui.TopGradientOverlay
 import com.trm.cryptosphere.core.ui.VerticalFeedPager
 import com.trm.cryptosphere.core.ui.rememberTokenCarouselSharedContentState
 
@@ -25,42 +29,47 @@ fun SharedTransitionScope.TokenFeedContent(
   modifier: Modifier = Modifier,
 ) {
   Scaffold(modifier = modifier) { paddingValues ->
-    VerticalFeedPager(
-      pagerState = rememberPagerState(pageCount = component.tokenFeedItems::size),
-      modifier = Modifier.fillMaxSize().padding(paddingValues),
-    ) { page ->
-      Card(modifier = Modifier.fillMaxSize()) {
-        val item = component.tokenFeedItems[page]
+    Box(modifier = Modifier.fillMaxSize().padding(paddingValues)) {
+      VerticalFeedPager(
+        pagerState = rememberPagerState(pageCount = component.tokenFeedItems::size),
+        modifier = Modifier.fillMaxSize(),
+      ) { page ->
+        Card(modifier = Modifier.fillMaxSize()) {
+          val item = component.tokenFeedItems[page]
 
-        TokenCarousel(
-          tokens = component.tokenCarouselConfig.items,
-          onItemClick = { index ->
-            // TODO: navigate to another token feed page
-          },
-          modifier =
-            Modifier.sharedElement(
-              sharedContentState =
-                rememberTokenCarouselSharedContentState(
-                  if (page == 0) component.tokenCarouselConfig.parentSharedElementId ?: item
-                  else item
-                ),
-              animatedVisibilityScope = animatedVisibilityScope,
-            ),
-        )
-
-        Text(
-          text = item,
-          style = MaterialTheme.typography.headlineLarge,
-          modifier =
-            Modifier.padding(16.dp)
-              .sharedElement(
-                sharedContentState = rememberSharedContentState("token-symbol-$item"),
+          TokenCarousel(
+            tokens = component.tokenCarouselConfig.items,
+            onItemClick = { index ->
+              // TODO: navigate to another token feed page
+            },
+            modifier =
+              Modifier.sharedElement(
+                sharedContentState =
+                  rememberTokenCarouselSharedContentState(
+                    if (page == 0) component.tokenCarouselConfig.parentSharedElementId ?: item
+                    else item
+                  ),
                 animatedVisibilityScope = animatedVisibilityScope,
               ),
-        )
+          )
+
+          Text(
+            text = item,
+            style = MaterialTheme.typography.headlineLarge,
+            modifier =
+              Modifier.padding(16.dp)
+                .sharedElement(
+                  sharedContentState = rememberSharedContentState("token-symbol-$item"),
+                  animatedVisibilityScope = animatedVisibilityScope,
+                ),
+          )
+        }
+        // TODO: navigation toolbar at the bottom
       }
 
-      // TODO: navigation toolbar at the bottom
+      TopGradientOverlay(modifier = Modifier.align(Alignment.TopCenter))
+
+      BottomGradientOverlay(modifier = Modifier.align(Alignment.BottomCenter))
     }
   }
 }
