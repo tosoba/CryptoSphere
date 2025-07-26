d3c4b89447cd689358efd4754b45c16faea7912f
merengue <therealmererengue@proton.me>
2025-07-26 13:35:08 +0200

    Remove deprecated windowWidthSizeClass usage

--- composeApp/src/androidMain/kotlin/com/trm/cryptosphere/core/ui/NavigationSuiteLayoutType.kt
+++ composeApp/src/androidMain/kotlin/com/trm/cryptosphere/core/ui/NavigationSuiteLayoutType.kt
@@ -3,21 +3,13 @@ package com.trm.cryptosphere.core.ui
 import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteType
 import androidx.compose.runtime.Composable
-import androidx.window.core.layout.WindowWidthSizeClass
 
 @Composable
 fun navigationSuiteLayoutType(): NavigationSuiteType =
   with(currentWindowAdaptiveInfo()) {
     when {
-      windowPosture.isTabletop -> {
-        NavigationSuiteType.Companion.NavigationBar
-      }
-      windowSizeClass.windowWidthSizeClass == WindowWidthSizeClass.Companion.EXPANDED ||
-        windowSizeClass.windowWidthSizeClass == WindowWidthSizeClass.Companion.MEDIUM -> {
-        NavigationSuiteType.Companion.NavigationRail
-      }
-      else -> {
-        NavigationSuiteType.Companion.NavigationBar
-      }
+      windowPosture.isTabletop -> NavigationSuiteType.Companion.NavigationBar
+      windowSizeClass.isWidthAtLeastBreakpoint(600) -> NavigationSuiteType.Companion.NavigationRail
+      else -> NavigationSuiteType.Companion.NavigationBar
     }
   }
