0f5dabcf78b6f1815bfad9e29f71f501f4d1f4fc
tosoba <therealmerengue@gmail.com>
2025-09-27 19:30:08 +0200

    Remove unused swift files

--- iosApp/iosApp/Core/Decompose/MutableValue.swift
+++
@@ -1,5 +0,0 @@
-import Shared
-
-func mutableValue<T: AnyObject>(_ initialValue: T) -> MutableValue<T> {
-    return MutableValueBuilderKt.MutableValue(initialValue: initialValue) as! MutableValue<T>
-}
