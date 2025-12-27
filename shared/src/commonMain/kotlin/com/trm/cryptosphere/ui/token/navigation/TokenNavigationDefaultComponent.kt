cc7acf5b01fdd4d6d6220dce4bc7c76d182e2f4e
tosoba <therealmerengue@gmail.com>
2025-12-27 13:07:15 +0100

    Extract TokenNavigationViewModel to a separate file

--- shared/src/commonMain/kotlin/com/trm/cryptosphere/ui/token/navigation/TokenNavigationDefaultComponent.kt
+++ shared/src/commonMain/kotlin/com/trm/cryptosphere/ui/token/navigation/TokenNavigationDefaultComponent.kt
@@ -9,7 +9,6 @@ import com.arkivanov.decompose.router.stack.popWhile
 import com.arkivanov.decompose.router.stack.pushToFront
 import com.arkivanov.decompose.value.Value
 import com.arkivanov.decompose.value.subscribe
-import com.arkivanov.essenty.instancekeeper.InstanceKeeper
 import com.arkivanov.essenty.instancekeeper.retainedInstance
 import com.trm.cryptosphere.core.base.AppCoroutineDispatchers
 import com.trm.cryptosphere.core.ui.TokenCarouselConfig
@@ -17,13 +16,6 @@ import com.trm.cryptosphere.domain.model.TokenItem
 import com.trm.cryptosphere.domain.repository.TokenHistoryRepository
 import com.trm.cryptosphere.domain.repository.TokenRepository
 import com.trm.cryptosphere.ui.token.feed.TokenFeedComponent
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
 
 class TokenNavigationDefaultComponent(
@@ -93,33 +85,4 @@ class TokenNavigationDefaultComponent(
   }
 
   @Serializable data class TokenFeedConfig(val tokenId: Int)
-}
-
-class TokenNavigationViewModel(
-  initialTokenIds: List<Int>,
-  private val tokenRepository: TokenRepository,
-  private val tokenHistoryRepository: TokenHistoryRepository,
-  dispatchers: AppCoroutineDispatchers,
-) : InstanceKeeper.Instance {
-  private val scope = CoroutineScope(dispatchers.main + SupervisorJob())
-
-  private val _tokenIds: MutableStateFlow<List<Int>> = MutableStateFlow(initialTokenIds)
-  val currentTokenId: Flow<Int?> = _tokenIds.map { it.lastOrNull() }
-  val tokens: Flow<List<TokenItem>> =
-    _tokenIds.map {
-      val tokens = tokenRepository.getTokensByIds(it).associateBy(TokenItem::id)
-      it.mapNotNull { tokenId -> tokens[tokenId] }
-    }
-
-  fun updateTokenIds(tokenIds: List<Int>) {
-    _tokenIds.value = tokenIds
-  }
-
-  fun addTokenToHistory(tokenId: Int) {
-    scope.launch { tokenHistoryRepository.addTokenToHistory(tokenId) }
-  }
-
-  override fun onDestroy() {
-    scope.cancel()
-  }
 }
---
+++ shared/src/commonMain/kotlin/com/trm/cryptosphere/ui/token/navigation/TokenNavigationViewModel.kt
@@ -0,0 +1,43 @@
+package com.trm.cryptosphere.ui.token.navigation
+
+import com.arkivanov.essenty.instancekeeper.InstanceKeeper
+import com.trm.cryptosphere.core.base.AppCoroutineDispatchers
+import com.trm.cryptosphere.domain.model.TokenItem
+import com.trm.cryptosphere.domain.repository.TokenHistoryRepository
+import com.trm.cryptosphere.domain.repository.TokenRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+
+class TokenNavigationViewModel(
+  initialTokenIds: List<Int>,
+  private val tokenRepository: TokenRepository,
+  private val tokenHistoryRepository: TokenHistoryRepository,
+  dispatchers: AppCoroutineDispatchers,
+) : InstanceKeeper.Instance {
+  private val scope = CoroutineScope(dispatchers.main + SupervisorJob())
+
+  private val _tokenIds: MutableStateFlow<List<Int>> = MutableStateFlow(initialTokenIds)
+  val currentTokenId: Flow<Int?> = _tokenIds.map { it.lastOrNull() }
+  val tokens: Flow<List<TokenItem>> =
+    _tokenIds.map {
+      val tokens = tokenRepository.getTokensByIds(it).associateBy(TokenItem::id)
+      it.mapNotNull { tokenId -> tokens[tokenId] }
+    }
+
+  fun updateTokenIds(tokenIds: List<Int>) {
+    _tokenIds.value = tokenIds
+  }
+
+  fun addTokenToHistory(tokenId: Int) {
+    scope.launch { tokenHistoryRepository.addTokenToHistory(tokenId) }
+  }
+
+  override fun onDestroy() {
+    scope.cancel()
+  }
+}
