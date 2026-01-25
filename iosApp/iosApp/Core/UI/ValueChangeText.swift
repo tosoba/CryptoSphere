3b892f33bf2ae57e6d2b5cc697eb824c890ec09a
tosoba <therealmerengue@gmail.com>
2026-01-25 19:22:19 +0100

    Use black text for positive value changes

--- iosApp/iosApp/Core/UI/ValueChangeText.swift
+++ iosApp/iosApp/Core/UI/ValueChangeText.swift
@@ -4,7 +4,7 @@ extension Text {
     func valueChangeText(isPositive: Bool) -> some View {
         font(.subheadline)
             .padding(.horizontal, 2)
-            .foregroundColor(isPositive ? .primary : .white)
+            .foregroundColor(isPositive ? .black : .white)
             .background(isPositive ? .green : .red)
             .clipShape(RoundedRectangle(cornerRadius: 4))
     }
